c3c7f189f83c0136cc72c9011f96371573dc023b
ahmed hussein <ahmedhussein0114278@gmail.com>
2022-03-14 00:15:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function ouch(){
+    if(document.getElementById("a").style.display!="none"){
+        document.getElementById("a").style.display="none";
+    }else{
+        document.getElementById("a").style.display="grid"
+    }
+}
